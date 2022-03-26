fd766b8ddc4e6a23584f5dd73029856d8d3d2d55
Paul Laffitte <plaffitt@gmail.com>
2022-03-26 23:26:27 +0100

    set command

--- src/shctx/cli.py
+++ src/shctx/cli.py
@@ -1,5 +1,18 @@
+import sys
+import yaml
+from shctx.context import get_context, start_context
+
 def list(config, args):
   pass
 
 def set(config, args):
-  pass
+  with open('config.yaml', 'r') as file:
+    config = yaml.safe_load(file)
+
+  context_name = args.set
+  if not context_name in config:
+    print('No such context: ' + context_name)
+    sys.exit(1)
+
+  context = get_context(context_name, config)
+  start_context(context_name, context)
