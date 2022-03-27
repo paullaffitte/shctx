472d6435de2b45493ac266885218ddd4e9f055db
Paul Laffitte <plaffitt@gmail.com>
2022-03-27 23:53:27 +0200

    store config.yaml in $HOME/.shctx directory
    closes #18

--- src/shctx/config.py
+++ src/shctx/config.py
@@ -1,16 +1,19 @@
 import yaml
 from pathlib import Path
 
-home = Path.home()
-user_directory = f'{home}/.shctx'
-
 def make_path(sub_path):
   return f'{user_directory}/{sub_path}'
 
 def get_config():
-  with open('config.yaml', 'r') as file:
+  with open(config_file, 'r') as file:
     config = yaml.safe_load(file)
+    if config == None:
+      config = {}
 
   config['_'] = {}
 
   return config
+
+home = Path.home()
+user_directory = f'{home}/.shctx'
+config_file = make_path('config.yaml')
--- src/shctx/main.py
+++ src/shctx/main.py
@@ -4,20 +4,26 @@
 from multiprocessing import context
 import sys
 import argparse
-import pathlib
+from pathlib import Path
 import shctx.cli as cli
-from shctx.config import get_config, user_directory
+import shctx.config as cfg
 from shctx.context import get_last_context
 
 def main():
-  pathlib.Path(user_directory).mkdir(exist_ok=True)
-
-  config = get_config()
+  Path(cfg.user_directory).mkdir(exist_ok=True)
+  Path(cfg.config_file).touch()
 
   parser = argparse.ArgumentParser()
   parser.add_argument("-l", "--list", action="store_true", help="context list")
   parser.add_argument("-s", "--set", help="set context")
+  parser.add_argument("-c", "--config", help="use another configuration file than the default one")
   args = parser.parse_args()
+
+  if args.config:
+    cfg.config_file = args.config
+    print('Using config file: ' + cfg.config_file)
+
+  config = cfg.get_config()
 
   if len(sys.argv) == 1:
     args.set = get_last_context()
